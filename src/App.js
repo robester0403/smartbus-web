29db1fd51eb1a3a4c0d183c1b6a03eed8a2ac55c
Fiora De Marco <fiorademarco7@gmail.com>
2022-11-30 16:32:08 -0500

    added NavHome component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import { CssBaseline } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Route, Routes } from "react-router";
 import Login from "./pages/Login";
+import NavHome from "./pages/NavHome/NavHome";
+import { LayoutContainer, TempPhoneContainer } from "./styles/styled";
 
 function App() {
   document.title = "Smartbus";
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <>
-        <CssBaseline />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </>
-    </QueryClientProvider>
+    <TempPhoneContainer>
+      <LayoutContainer>
+        <QueryClientProvider client={queryClient}>
+          <>
+            <CssBaseline />
+            <Routes>
+              <Route path='/login' element={<Login />} />
+              <Route path='/NavHome' element={<NavHome />} />
+            </Routes>
+          </>
+        </QueryClientProvider>
+      </LayoutContainer>
+    </TempPhoneContainer>
   );
 }
 
