b1455ec1f74c601e975ef71ae7623ad1cb3f4306
robester0403 <robkso@gmail.com>
2022-11-30 08:43:08 -0500

    form added. Bus rocking animation added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,35 @@
-import { Button } from "@mui/material";
+import { Button, Grid, TextField } from "@mui/material";
 import React from "react";
-import * as yup from "yup";
-import { useFormik } from "formik";
-import styled from "styled-components";
-import loginImg from "../assets/images/login.png";
+import styled, { keyframes } from "styled-components";
+import loginImg from "../assets/images/loginImage.jpg";
 
 const Login = () => {
   return (
     <TempPhoneContainer>
       <LayoutContainer>
-        <img src={loginImg} alt="login" />
-        <div>login</div>
-        <div>password</div>
-        <Button>Sign In</Button>
+        <Logo src={loginImg} alt="login" />
+        <form>
+          <Grid
+            container
+            alignItems="center"
+            justify="center"
+            direction="column"
+          >
+            <TextField
+              id="login-input"
+              name="login"
+              label="login"
+              sx={{ marginBottom: "32px" }}
+            />
+            <TextField
+              id="password-input"
+              name="password"
+              label="password"
+              sx={{ marginBottom: "32px" }}
+            />
+            <Button>Sign In</Button>
+          </Grid>
+        </form>
       </LayoutContainer>
     </TempPhoneContainer>
   );
@@ -30,4 +47,20 @@ const LayoutContainer = styled.section`
   flex-direction: column;
   align-items: center;
   box-sizing: border-box;
+`;
+
+const rockingBus = keyframes`
+  0%, 100% { transform: rotate(-.15deg); }
+  15% { transform: rotate(.20deg); }
+  20% {transform:scale(.9975);}
+  25% { transform: rotate(-.20deg); }
+  90% { transform: rotate(.15deg); }
+  80% {transform:scale(.9975);}
+  `;
+
+const Logo = styled.img`
+  width: 100%;
+  animation-name: ${rockingBus};
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
 `;
