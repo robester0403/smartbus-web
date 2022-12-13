f5ab56b20eebf2510e27513dd904431c4f25dbe5
Henry Luan <henryluan95@gmail.com>
2022-12-13 15:50:24 -0500

    Initilized a template for the pingHeartBeat function

--- src/pages/NavHome/NavHome.jsx
+++ src/pages/NavHome/NavHome.jsx
@@ -1,14 +1,47 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 const data = {
   username: "username",
   userIsDriver: false,
 };
+const HEARTBEAT_ENDPOINT = "";
 
 const NavHome = () => {
   const [user, setUser] = useState({});
+
+  const currentPosition = () => {
+    return new Promise((res, rej) => {
+      navigator.geolocation.getCurrentPosition(res, rej);
+    });
+  };
+
+  const pingHeartBeat = async () => {
+    const position = await currentPosition();
+
+    const heartBeatData = {
+      userId: 1, // Should be included in the Token
+      userRole: "Driver", // Should be included in the Token
+      timestamp: Date.now(),
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+      speed: 30,
+    };
+
+    axios.post(HEARTBEAT_ENDPOINT, heartBeatData);
+    // console.log(heartBeatData);
+  };
+
   useEffect(() => {
     setUser(data);
+
+    const heartBeatID = setInterval(() => {
+      pingHeartBeat();
+    }, 2000);
+    return () => {
+      clearInterval(heartBeatID);
+    };
   }, []);
+
   return (
     <div>
       {user?.userIsDriver && (
