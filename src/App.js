77d9780f2dad4acb9532080b3f750bcd01bba7f1
robester0403 <robkso@gmail.com>
2022-12-14 20:21:48 -0500

    Create Scaffolding of bare minimum pages we would need (some in the figma could be views), added a catch all route to the error page, added helmet

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import React from "react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 import { CssBaseline } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Route, Routes } from "react-router";
 import NavHome from "./pages/NavHome/NavHome";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
+import RouteDetails from "./pages/RouteDetails/RouteDetails";
+import SetupPage from "./pages/SetupPage/SetupPage.jsx";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import IntroPage from "./pages/IntroPage/IntroPage";
 import { LayoutContainer, TempPhoneContainer } from "./AppStyle";
 
 function App() {
   document.title = "Smartbus";
   const queryClient = new QueryClient();
   return (
-    <TempPhoneContainer>
-      <LayoutContainer>
-        <QueryClientProvider client={queryClient}>
-          <>
-            <CssBaseline />
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/NavHome" element={<NavHome />} />
-            </Routes>
-          </>
-        </QueryClientProvider>
-      </LayoutContainer>
-    </TempPhoneContainer>
+    <HelmetProvider>
+      <TempPhoneContainer>
+        <LayoutContainer>
+          <QueryClientProvider client={queryClient}>
+            <Helmet>
+              <title>Learning React Helmet!</title>
+              <meta name="SmartBus" content="The only bus app you will need." />
+            </Helmet>
+            <>
+              <CssBaseline />
+              <Routes>
+                <Route path="/" element={<IntroPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/navhome" element={<NavHome />} />
+                <Route path="/routedetails" element={<RouteDetails />} />
+                <Route path="/setuppage  " element={<SetupPage />} />
+                <Route path="/errorpage" element={<ErrorPage />} />
+                <Route path="*" element={<ErrorPage />} />
+              </Routes>
+            </>
+          </QueryClientProvider>
+        </LayoutContainer>
+      </TempPhoneContainer>
+    </HelmetProvider>
   );
 }
 
