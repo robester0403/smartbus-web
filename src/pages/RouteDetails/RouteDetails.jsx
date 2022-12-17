ab1148002347d7a643ac1d1d8956904c3787a582
Henry Luan <henryluan95@gmail.com>
2022-12-17 17:42:23 -0500

    Change timestamp to a string type as per back-end team request

--- src/pages/RouteDetails/RouteDetails.jsx
+++ src/pages/RouteDetails/RouteDetails.jsx
@@ -24,7 +24,7 @@ const RouteDetails = () => {
       const heartBeatData = {
         userId: 1, //To be replaced with userId
         userRole: "driver", //To be replaced with userRole variable
-        timestamp: Date.now(),
+        timestamp: Date.now().toString(),
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
         speed: position.coords.speed,
